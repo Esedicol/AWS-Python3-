df003aaae0e800faa96ee435373b345f3331f474
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-09-01 17:43:21 +0100

    menu finish

--- menu.py
+++ menu.py
@@ -95,7 +95,153 @@ def instance_menu():
       instance_menu()
 
  
+import buckets
+ # ------------------------------ bucket menu -------------------------------
+def bucket_menu():
+  os.system('clear')
+  while True:  
+    print ('''
+     ===========================================
+                      Buckets
+     ===========================================
+     ''')
+    print ("Select one the following to start:")
+    print ("1. Create Bucket")
+    print ("2. Add Files into Bucket")
+    print ("3. List Buckets")
+    print ("4. Delete Bucket")
+    print ("5. Delete Bucket Contents")
+    print ("\n9. Back")
+    print ("\n0. Quit")
 
+    choice = input(" >>>  ")
+    
+    if choice == '1':
+      buckets.create_bucket()
+    elif choice == '2':
+      buckets.add_contents()
+    elif choice == '3':
+      buckets.list_buckets()
+    elif choice == '4':
+      buckets.delete_buckets()
+    elif choice == '5':
+      buckets.delete_contents()
+    elif choice == '9':
+      back()
+    elif choice == '0':
+      exit()
+    else:
+      bucket_menu()
+
+
+import nginx
+# -------------------------- NGINX --------------------------------------
+def nginx_menu():
+  os.system('clear')
+  while True:  
+    print ('''
+     ===========================================
+                      NGINX
+     ===========================================
+     ''')
+    print ("Select one the following to start:")
+    print ("1. Check if NGINX is running")
+    print ("2. Start NGINX")
+    print ("\n9. Back")
+    print ("\n0. Quit")
+
+    choice = input (" >>> ")
+
+    if choice == '1':
+      nginx.checknginx()
+    elif choice == '2':
+      nginx.startnginx()
+    elif choice == '9':
+      back()
+    elif choice == '0':
+      exit()
+
+
+# -------------------------- keys --------------------------------------
+def key():
+  os.system('clear')
+  while True:  
+    print ('''
+     ===========================================
+                      Keys
+     ===========================================
+     ''')
+    print ("Select one the following to start:")
+    print ("1. List all Keys")
+    print ("2. Create new key (.pem)")
+    print ("3. Delete key")
+    print ("\n9. Back")
+    print ("\n0. Quit")
+
+    choice = input (" >>> ")
+
+    if choice == '1':
+      instance.list_keys()
+    elif choice == '2':
+      instance.new_key()
+    elif choice == '3':
+      instance.delete_keys()
+    elif choice == '9':
+      back()
+    elif choice == '0':
+      exit()
+
+# -------------------------- file system --------------------------------------
+def file():
+  os.system('clear')
+  while True:  
+    print ('''
+     ===========================================
+                      File System
+     ===========================================
+     ''')
+    print ("Select one the following to start:")
+    print ("1. Add file into instance")
+    print ("2. List all Files")
+    print ("3. Create file")
+    print ("4. Delete file")
+
+    print ("\n9. Back")
+    print ("\n0. Quit")
+
+    choice = input (" >>> ")
+
+    if choice == '1':
+      instance.add_file()
+    elif choice == '2':
+      instance.list_file()
+    elif choice == '3':
+      instance.create_file()
+    elif choice == '4':
+      instance.del_file()
+    elif choice == '9':
+      back()
+    elif choice == '0':
+      exit()
+
+
+
+
+ 
+# Back to main menu
+def back():
+  main_menu()
+ 
+# Exit program
+def exit():
+  sys.exit()
+ 
+
+
+
+
+def main():
+  main_menu()
 
 if __name__ == '__main__':
   main()
