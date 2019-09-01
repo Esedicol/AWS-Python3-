8f510e09cd86f6b8a901b2552ddb16925488a039
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-09-01 17:31:56 +0100

    menu is build up using an existing template which i got from https://www.bggofurther.com/2015/01/create-an-interactive-command-line-menu-using-python/

---
+++ menu.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''
+code structure implemented from:
+https://www.bggofurther.com/2015/01/create-an-interactive-command-line-menu-using-python/
+'''
+import sys
+import os
+import boto3
+
+ 
+# Main definition - constants
+menu_actions  = {}  
+
+# --------------------------------- main menu -----------------------------
+def main_menu():
+  os.system('clear')
+  print('')
+  print('''           \ \ \      /  __| 
+          _ \ \ \ \  / \__ \  Amazon Web Services
+        _/  _\ \_/\_/  ____/ ''')
+
+  print ('''
+  ================================================
+         WELCOME to my AWS Automation System
+  ================================================
+  ''')
+  print ("Select one the following to start:")
+  print ("1. Instances")
+  print ("2. Buckets")
+  print ("3. File System")
+  print ("4. NGINX")
+  print ("5. Keys")
+  print ("\n0. Quit")
+
+  choice = input(" >>>  ")
+  if (choice == '1'):
+    instance_menu()
+  elif choice == '2':
+    bucket_menu()
+  elif choice == '3':
+    file()
+  elif choice == '4':
+    nginx_menu()
+  elif choice == '5':
+    key()
+  elif choice == '0':
+    sys.exit()
+  else:
+    main_menu()
+
+  return
+
